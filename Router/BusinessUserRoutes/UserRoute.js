a7e25b4ac0a211b4f6f11964e7cea36e9345b06c
vikascodesoftic455 <vikas.codesoftic@gmail.com>
2022-01-13 11:08:25 +0530

    validation for signup user

--- Router/BusinessUserRoutes/UserRoute.js
+++ Router/BusinessUserRoutes/UserRoute.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { check } = require('express-validator')
 const UserController  = require('../../Controllers/UserController/UserController')
 
 const router = express.Router()
@@ -14,7 +15,32 @@ router
 // This are Signup Route  
 router
    .route('/signup')
-   .post(UserController.signup)     
+   .post(
+         [
+             check('name').isEmpty(),
+             check('username').isEmpty().isEmpty(),
+             check('email').isEmail().withMessage({
+                    message: 'Not an email',
+                    errorCode: 1,
+                }),
+            check('phoneNumber').isNumeric().isEmpty(),
+            check('AlterNativeNumber').isNumeric().isEmpty(),
+            check('password', 'The password must be 8+ chars long and contain a number')
+                .not()
+                .isIn(['123', 'password', 'god'])
+                .withMessage('Do not use a common word as the password')
+                .isLength({ min: 8 })
+                .matches(/\d/),
+            check('PasswordCofirm', 'The PasswordCofirm must be 8+ chars long and contain a number')
+                .not()
+                .isIn(['123', 'PasswordCofirm', 'god'])
+                .withMessage('Do not use a common word as the password')
+                .isLength({ min: 8 })
+                .matches(/\d/),    
+            
+         ],
+         UserController.signup
+        )     
 
 
 
